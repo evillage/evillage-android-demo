2d65f81a3ec46539f511790621539b7cfb9dcd8a
Otto Boekesteijn <oboekesteijn@worth.systems>
2020-03-26 08:44:40 +0100

    Update .gitignore file and get latest changes in

--- app/src/main/java/nl/evillage/notifications/ClangFirebaseMessagingService.kt
+++ app/src/main/java/nl/evillage/notifications/ClangFirebaseMessagingService.kt
@@ -19,8 +19,8 @@ import kotlin.random.Random
 open class ClangFirebaseMessagingService : FirebaseMessagingService() {
     lateinit var clang: Clang
 
-    override fun onMessageReceived(p0: RemoteMessage?) {
-        p0?.data?.let { data ->
+    override fun onMessageReceived(p0: RemoteMessage) {
+        p0.data.let { data ->
             when (data["type"]) {
                 "clang" -> {
                     if (handleClangNotification(data)) return
@@ -46,14 +46,20 @@ open class ClangFirebaseMessagingService : FirebaseMessagingService() {
             putExtra("keyValue", list)
         }
         val pendingIntent: PendingIntent =
-            PendingIntent.getActivity(this, systemNotificationId, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            PendingIntent.getActivity(
+                this,
+                systemNotificationId,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
-        val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val channelId = "channel-01"
         val channelName = "Channel Name"
-        val importance = NotificationManager.IMPORTANCE_HIGH
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
             val mChannel = NotificationChannel(
                 channelId, channelName, importance
             )
@@ -89,14 +95,14 @@ open class ClangFirebaseMessagingService : FirebaseMessagingService() {
 
             actionId?.let {
                 actionTitle.let {
-                    val pendingIntent =
-                        PendingIntent.getService(this, Random.nextInt(0, Int.MAX_VALUE) + i,
-                            Intent(this, ClangIntentService::class.java).apply {
-                                this.putExtra("notificationId", notId)
-                                this.putExtra("actionId", actionId)
-                                this.putExtra("systemNotificationId", systemNotificationId)
-                            }, PendingIntent.FLAG_UPDATE_CURRENT
-                        )
+                    val pendingIntent = PendingIntent.getService(
+                        this, Random.nextInt(0, Int.MAX_VALUE) + i,
+                        Intent(this, ClangIntentService::class.java).apply {
+                            this.putExtra("notificationId", notId)
+                            this.putExtra("actionId", actionId)
+                            this.putExtra("systemNotificationId", systemNotificationId)
+                        }, PendingIntent.FLAG_UPDATE_CURRENT
+                    )
 
                     notificationBuilder.addAction(
                         android.R.drawable.ic_notification_overlay,
@@ -108,17 +114,20 @@ open class ClangFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
-    override fun onNewToken(token: String?) {
-        token?.let { fbToken ->
-            clang = Clang.getInstance(applicationContext,"46b6dfb6-d5fe-47b1-b4a2-b92cbb30f0a5", "63f4bf70-2a0d-4eb2-b35a-531da0a61b20")
-            clang.updateToken(fbToken,
-                {
-                    Log.d("TAG", "Refreshed token: $fbToken")
-                },
-                {
-                    Log.d("TAG", "failed to update token")
-                }
-            )
-        }
+    override fun onNewToken(token: String) {
+        clang = Clang.getInstance(
+            applicationContext,
+            "46b6dfb6-d5fe-47b1-b4a2-b92cbb30f0a5",
+            "63f4bf70-2a0d-4eb2-b35a-531da0a61b20"
+        )
+        clang.updateToken(token,
+            {
+                Log.d("TAG", "Refreshed token: $token")
+            },
+            {
+                Log.d("TAG", "failed to update token")
+            }
+        )
+
     }
 }
